352920e8a97b9a284f1dd3f133f179bb47b72675
mikhailkaryamin <karyamin.mihail@yandex.ru>
2020-08-21 19:01:30 +0500

    refacor(route-private): change folder and rename => private

---
+++ src/routers/private/private.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import {
+  Redirect,
+  Route,
+} from 'react-router-dom';
+import {
+  bool,
+  string,
+  func
+} from 'prop-types';
+
+import {AppRoute} from '../../const.js';
+
+const Private = (props) => {
+  const {
+    exact,
+    isAuth,
+    path,
+    render,
+  } = props;
+
+  return (
+    <Route
+      exact={exact}
+      path={path}
+      render={(routeProps) => {
+        return (
+          isAuth
+            ? render(routeProps)
+            : <Redirect to={{
+              pathname: AppRoute.LOGIN,
+              state: {
+                from: routeProps.location
+              }
+            }}
+            />
+        );
+      }}
+    />
+  );
+};
+
+Private.propTypes = {
+  exact: bool.isRequired,
+  isAuth: bool.isRequired,
+  path: string.isRequired,
+  render: func.isRequired,
+};
+
+export default Private;
